5ad996be28dd375115e705b92ec8485ab6dfb358
gsudarshan1990 <sudarshan2009@live.in>
2025-12-07 09:51:18 +0530

    Navigate To Home Page After Login

--- PlaywrightBasicsTypeScript/src4/pages/AccountsOverviewPage.po.ts
+++ PlaywrightBasicsTypeScript/src4/pages/AccountsOverviewPage.po.ts
@@ -1,16 +1,19 @@
 import { Page, Locator} from '@playwright/test';
 import { expect } from '@playwright/test';
+import { ParaBankClick } from '../Helper/CommonHelper';
 
 export class AccountsOverview{
     readonly page: Page;
     readonly heading: Locator;
     readonly table: Locator;
+    readonly home: Locator;
 
     constructor(page:Page)
     {
         this.page = page;
         this.heading = page.locator('#showOverview h1');
         this.table = page.locator('#accountTable');
+        this.home = page.locator("#headerPanel>ul[class='button']>li[class='home']");
     }
 
     async verifyLoaded()
@@ -18,14 +21,8 @@ export class AccountsOverview{
        await expect(this.heading).toHaveText('Accounts Overview');
     }
 
-    async getAccountNumber(): Promise<string[]>
+    async navigateToHomePage()
     {
-       const accountnumbers: string[] = [];
-       await this.page.waitForTimeout(4000);
-       const rows : Locator = this.table.locator('tbody tr td a');
-       console.log(await rows.first().textContent());
-       console.log(await rows.nth(1).textContent());
-       return accountnumbers;
-        
+        await ParaBankClick(this.home);
     }
 }
--- PlaywrightBasicsTypeScript/src4/tests/ParaBankLogin.spec.ts
+++ PlaywrightBasicsTypeScript/src4/tests/ParaBankLogin.spec.ts
@@ -4,13 +4,27 @@ import { AccountsOverview } from '../pages/AccountsOverviewPage.po';
 
 let parabankloginpage : ParaBankLoginPage
 let accountoverview : AccountsOverview
-test('Login to the bank account', async({page})=>{
+
+test.beforeEach(async({page})=>{
         const parabankloginpage = new ParaBankLoginPage(page);
         await parabankloginpage.navigate();
         await page.waitForSelector("input[name='username']");
-        await parabankloginpage.BankLogin('mark_test_2025_004','suresh08');
-        await page.waitForTimeout(2000);
+        await parabankloginpage.BankLogin('mark_test_2025_007','suresh08');
+        await page.waitForLoadState('load');
+
+});
+
+test('Login to the bank account', async({page})=>{
         await expect(page).toHaveURL('https://parabank.parasoft.com/parabank/overview.htm'); 
         const accountoverview =new AccountsOverview(page);
         accountoverview.verifyLoaded();
+});
+
+test('Navigate to Home page after Login',async({page})=>{
+        await page.waitForSelector('#showOverview');
+        await expect(page).toHaveTitle("ParaBank | Accounts Overview");
+        const accountoverview =new AccountsOverview(page);
+        accountoverview.navigateToHomePage();
+        await page.waitForSelector("#rightPanel>span.services");
+        await expect(page).toHaveURL('https://parabank.parasoft.com/parabank/index.htm');       
 });
