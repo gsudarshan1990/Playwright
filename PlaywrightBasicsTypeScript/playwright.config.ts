f6b09a913c5f01534eeed9192ae2ef5b1b9f9fac
gsudarshan1990 <sudarshan2009@live.in>
2025-12-23 21:55:23 +0530

    Login and Logout

--- PlaywrightBasicsTypeScript/playwright.config.ts
+++ PlaywrightBasicsTypeScript/playwright.config.ts
@@ -36,12 +36,17 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-   {
+    {
+       name: 'Google Chrome',
+       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+     },
+   /*{
       name: 'chromium',
       use: { ...devices['Desktop Chrome'],
       headless: false
        },
     },
+    */
     /*
     {
       name: 'firefox',
@@ -68,10 +73,7 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+     
   ],
 
   /* Run your local dev server before starting the tests */
