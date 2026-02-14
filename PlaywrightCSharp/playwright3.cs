be83db0b0e5c7a8a49bfa3d0967afe86ec9638ba
Sudarshan <sudarshan2009@live.in>
2026-02-14 16:25:31 +0530

    Add files via upload

---
+++ PlaywrightCSharp/playwright3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Playwright;
+
+namespace Playwright_Practice
+{	
+	public class playwright3
+	{
+		private IBrowser _browser;
+
+		private IPage _page;
+
+		[Fact]
+				
+		public async Task LauchCricInfo()
+		{
+			//Launching the playwright object
+			using var playwright = await Playwright.CreateAsync();
+
+			//Launch Options
+
+			var launchOptions = new BrowserTypeLaunchOptions { Headless = false };
+
+			//Add launchoption to the browser 
+
+			_browser = await playwright.Chromium.LaunchAsync(launchOptions);
+
+			// Launch the page
+
+			_page = await _browser.NewPageAsync();
+
+			await _page.GotoAsync("https://www.espncricinfo.com/");
+
+			await Task.Delay(2000);
+
+		}
+
+	}
+}
---
+++ PlaywrightCSharp/playwright4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Playwright;
+
+namespace Playwright_Practice
+{
+	public class playwright4 : IAsyncLifetime
+	{
+		private IPlaywright _playwright = null!;
+
+		private IBrowser _browser =null!;
+
+		private IPage _page = null!;
+		public async Task InitializeAsync()
+		{
+			_playwright = await Playwright.CreateAsync();
+
+			_browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+
+			_page = await _browser.NewPageAsync();
+
+		}
+
+		[Fact]
+
+		public async Task LaunchWebsite()
+		{
+			await _page.GotoAsync("https://playwright.dev/");
+			string title = await _page.TitleAsync();
+			Assert.Contains("Playwright", title);
+			await _page.GetByRole(AriaRole.Link, new() { Name ="Get Started"}).ClickAsync();
+			string url = _page.Url;
+			Assert.Contains("/docs", url);
+			
+		}
+
+		public async Task DisposeAsync()
+		{
+			await _browser.DisposeAsync();
+			_playwright.Dispose();
+			
+		}
+
+		
+	}
+}
