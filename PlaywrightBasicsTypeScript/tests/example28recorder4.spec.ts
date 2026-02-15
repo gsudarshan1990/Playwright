97b0c44dcc73f7cd820f6ce4f0875e6fa05edafe
gsudarshan1990 <sudarshan2009@live.in>
2026-02-15 17:26:47 +0530

    Unexpected deletion

--- PlaywrightBasicsTypeScript/tests/example28recorder4.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/inputs');
-  await page.getByRole('spinbutton').click();
-  await page.getByRole('spinbutton').fill('12345');
-  await expect(page.getByRole('spinbutton')).toHaveValue('12345');
-});
