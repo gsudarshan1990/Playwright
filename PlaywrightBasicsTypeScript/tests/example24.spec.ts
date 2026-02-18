9ab0697a77c188108f0ed16a5f4f6707bc0c05b6
gsudarshan1990 <sudarshan2009@live.in>
2026-02-18 19:16:26 +0530

    Worked on the Recording and Unexpected deletion

--- PlaywrightBasicsTypeScript/tests/example24.spec.ts
+++ PlaywrightBasicsTypeScript/tests/example24.spec.ts
@@ -1,10 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('Playwright basic test',async({page})=>{
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-    await page.getByRole('link',{name:'Get Started'}).click();
-    await expect(page).toHaveURL(/.*docs/);
-
-
+test('test', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('tomsmith');
+  await page.getByRole('textbox', { name: 'Username' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: ' Login' }).click();
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area! ×');
 });
