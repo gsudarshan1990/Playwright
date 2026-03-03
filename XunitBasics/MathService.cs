3a58b77a870bc21af99d6f8bdbfc8f75674b547c
gsudarshan1990 <sudarshan2009@live.in>
2026-03-03 12:50:26 +0530

    Xunit ITestOutputHelper and Running from the command line

---
+++ XunitBasics/MathService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XunitBasics
+{
+	public class MathService
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
