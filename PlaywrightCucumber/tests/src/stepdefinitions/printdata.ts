0a300c963bc7ecb420615d8a518e5cc8244ba79f
Sudarshan <sudarshan2009@live.in>
2026-05-18 10:39:52 +0000

    First Playwright Typescript Cucumber

---
+++ PlaywrightCucumber/tests/src/stepdefinitions/printdata.ts
@@ -0,0 +1,19 @@
+import {Given, When,Then } from '@cucumber/cucumber'
+       
+         Given('User in present on register page', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log("User in present on teh registration page")
+         });
+       
+ 
+         When('user used its own credentials', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log("user used its own credentials")
+         });
+       
+   
+         Then('Otp will be received', function () {
+           // Write code here that turns the phrase above into concrete actions
+           console.log("otp is receive")
+         });
+       
