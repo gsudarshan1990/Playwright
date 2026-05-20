f5a7bef949062c8c58b085533d57684bb5e85b60
gsudarshan1990 <sudarshan2009@live.in>
2026-05-20 15:40:28 +0530

    Playwright Typescript Grouping

---
+++ Playwright_Typescript_May/tests/testgrouping.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect} from '@playwright/test'
+
+test.describe('Login to the sauce demo',()=>{
+
+    test.beforeEach(async ({page})=>{
+        await page.goto('https://www.saucedemo.com/')
+
+    })
+
+    test('Login first user', async ({page})=>{
+
+        await page.getByPlaceholder('Username').fill('standard_user')
+        await page.getByRole('textbox',{'name':'Password'}).fill('secret_sauce')
+        await page.getByRole('button').click()
+        expect (page.url()).toBe('https://www.saucedemo.com/inventory.html')
+        expect (page.getByText('Swag Labs')).toBeVisible()
+    })
+
+    test('Failed Login',async  ({page})=>{
+
+        await page.getByPlaceholder('Username').fill('locked_out_user')
+        await page.getByRole('textbox',{'name':'Password'}).fill('secret_sauce')
+        await page.getByRole('button').click()
+        expect (page.getByText('Epic sadface: Sorry, this user has been locked out.')).toBeVisible()
+    })
+})
